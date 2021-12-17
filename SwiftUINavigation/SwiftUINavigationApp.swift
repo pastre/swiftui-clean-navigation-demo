ed34ed48634c4349971f201a5ea3f3bd0b15f953
Bruno Pastre <pastre68@gmail.com>
2021-12-17 10:09:25 -0300

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Bruno Pastre on 17/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
